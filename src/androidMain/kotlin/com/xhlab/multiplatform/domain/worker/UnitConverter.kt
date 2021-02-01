a1e37b55d757b9ad31aa3b4515fa9437bf31c100
wlghdu97 <inganlegi@gmail.com>
2021-02-01 18:43:29 +0900

    Add UnitConverter

---
+++ src/androidMain/kotlin/com/xhlab/multiplatform/domain/worker/UnitConverter.kt
@@ -0,0 +1,6 @@
+package com.xhlab.multiplatform.domain.worker
+
+class UnitConverter : DataConverter<Unit> {
+    override fun convert(from: Unit?): Unit? = null
+    override fun convertBack(from: Any?): Unit? = null
+}
