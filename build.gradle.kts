7edda8cd2974abddc9efa199c8cf10c6565d7fce
wlghdu97 <inganlegi@gmail.com>
2021-02-01 12:08:49 +0900

    Change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 val artifactName = "infrastructure"
 val artifactGroup = "com.xhlab.mobile"
-val artifactVersion = "0.2.2"
+val artifactVersion = "0.2.3"
 
 group = artifactGroup
 version = artifactVersion
