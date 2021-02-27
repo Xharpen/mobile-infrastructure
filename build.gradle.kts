1c159a12173f971eaecb7cd205b2283b9b583ed9
wlghdu97 <inganlegi@gmail.com>
2021-02-27 16:37:57 +0900

    Relocate infrastructure module inside of parent module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,139 +1,15 @@
-plugins {
-    id("com.android.library")
-    kotlin("multiplatform") version "1.4.20"
-    id("kotlin-android-extensions")
-    id("maven-publish")
-}
-
-val artifactName = "infrastructure"
-val artifactGroup = "com.xhlab.mobile"
-val artifactVersion = "0.2.7"
-
-group = artifactGroup
-version = artifactVersion
-
-repositories {
-    google()
-    mavenCentral()
-    jcenter()
-}
-
-kotlin {
-    android {
-        publishLibraryVariants("release", "debug")
-    }
-    ios {
-        binaries {
-            framework {
-                baseName = artifactName
-            }
-        }
-    }
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1-native-mt")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val androidMain by getting {
-            dependencies {
-                implementation("androidx.core:core-ktx:1.3.2")
-                implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0-alpha02")
-                implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.0-alpha02")
-                implementation("androidx.work:work-runtime-ktx:2.5.0")
-            }
-        }
-        val androidTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-                implementation("junit:junit:4.13")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.2")
-            }
-        }
-        val iosMain by getting
-        val iosTest by getting
-    }
-}
-
-android {
-    compileSdkVersion(30)
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    defaultConfig {
-        minSdkVersion(21)
-        targetSdkVersion(30)
-    }
-
-    sourceSets {
-        getByName("main") {
-            manifest.srcFile("src/androidMain/AndroidManifest.xml")
-            java.srcDirs("src/androidMain/kotlin")
-            res.srcDirs("src/androidMain/res")
-        }
-        getByName("test") {
-            java.srcDirs("src/androidTest/kotlin")
-            res.srcDirs("src/androidTest/res")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-}
-
-publishing {
-    publications.withType<MavenPublication>().forEach {
-        it.pom {
-            name.set("Xharpen Mobile infrastructure")
-            description.set("Infrastructure classes for base architectures")
-            url.set("https://github.com/Xharpen/mobile-infrastructure")
-
-            withXml {
-                fun groovy.util.Node.addDependency(dependency: Dependency, scope: String) {
-                    appendNode("dependency").apply {
-                        appendNode("groupId", dependency.group)
-                        appendNode("artifactId", dependency.name.toLowerCase())
-                        appendNode("version", dependency.version)
-                        appendNode("scope", scope)
-                    }
-                }
-            }
-        }
-    }
+buildscript {
 
     repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Xharpen/mobile-infrastructure")
-            credentials {
-                username = System.getenv("PUBLISH_USERNAME")
-                password = System.getenv("PUBLISH_TOKEN")
-            }
-        }
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("com.android.tools.build:gradle:4.1.2")
     }
 }
 
-afterEvaluate {
-    project.publishing.publications.withType<MavenPublication>().all {
-        groupId = group.toString()
-        version = version
-        artifactId = when {
-            name.contains("metadata") -> "$artifactName-common"
-            name.contains("kotlinMultiplatform") -> artifactName
-            else -> "$artifactName-$name"
-        }.toLowerCase()
-    }
-}
+group = "com.xhlab.mobile"
