368e722553699158d56f7706608a3a44757be084
wlghdu97 <inganlegi@gmail.com>
2021-02-27 15:16:38 +0900

    Fix UnitConverter to return Unit as parameter

--- src/androidMain/kotlin/com/xhlab/multiplatform/domain/worker/UnitConverter.kt
+++ src/androidMain/kotlin/com/xhlab/multiplatform/domain/worker/UnitConverter.kt
@@ -2,5 +2,5 @@ package com.xhlab.multiplatform.domain.worker
 
 class UnitConverter : DataConverter<Unit> {
     override fun convert(from: Unit?): Unit? = null
-    override fun convertBack(from: Any?): Unit? = null
+    override fun convertBack(from: Any?): Unit = Unit
 }
