817341955797e8fd0d68cadab151290d5960efa1
wlghdu97 <inganlegi@gmail.com>
2021-06-25 16:57:08 +0900

    Fix EventFlow channel suspension

--- multiplatform/src/commonMain/kotlin/com/xhlab/multiplatform/util/EventFlow.kt
+++ multiplatform/src/commonMain/kotlin/com/xhlab/multiplatform/util/EventFlow.kt
@@ -1,18 +1,32 @@
 package com.xhlab.multiplatform.util
 
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.transform
 
 /**
  * Special Flow for replacement of LiveEvent.
  */
 class EventFlow<T> {
-    private val channel = Channel<T>()
+    private val internalFlow = MutableStateFlow<EventWrapper<T>>(EventWrapper(null))
 
-    val flow: Flow<T> = channel.receiveAsFlow()
+    val flow: Flow<T> = internalFlow.transform {
+        if (it.wrappedValue != null) {
+            emit(it.wrappedValue)
+        }
+    }
 
-    suspend fun emit(value: T) {
-        channel.send(value)
+    fun emit(value: T) {
+        internalFlow.value = EventWrapper(value)
+    }
+
+    private class EventWrapper<T>(val wrappedValue: T? = null) {
+        override fun equals(other: Any?): Boolean {
+            return false
+        }
+
+        override fun hashCode(): Int {
+            return wrappedValue?.hashCode() ?: 0
+        }
     }
 }
